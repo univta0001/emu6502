72f2c6c423f551c7d2d5b51f11e5d09039736b7b
univta0001 <univta0001@gmail.com>
2022-12-28 00:07:19 +0800

    Updated profile set script

--- profile_set.bat
+++ profile_set.bat
@@ -1,3 +1,4 @@
 @echo off
 @llvm-profdata merge -o d:/temp/emu6502/merged_profdata.bin d:/temp/pgodata
 @set RUSTFLAGS=-Cprofile-use=d:/temp/emu6502/merged_profdata.bin
+@cargo build --release --target=x86_64-pc-windows-msvc
