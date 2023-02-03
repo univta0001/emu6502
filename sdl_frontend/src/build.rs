ffb8127a30e319aa85a04fc39b25b09410b05039
univta0001 <univta0001@gmail.com>
2023-02-03 11:35:27 +0800

    Improve git hash implementation for scenario when it cannot be obtained

--- sdl_frontend/src/build.rs
+++ sdl_frontend/src/build.rs
@@ -5,7 +5,11 @@ use std::process::Command;
 fn main() {
     embed_resource::compile("res/emu6502.rc");
 
-    let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+    let result= Command::new("git").args(&["rev-parse", "HEAD"]).output();
+    let git_hash = if let Ok(output) = result {
+        String::from_utf8(output.stdout).unwrap()
+    } else {
+        "Unknown".into()
+    };
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);    
 }
