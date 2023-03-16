2f56158d2c20eef872fc14636c3c461921767932
univta0001 <univta0001@gmail.com>
2023-03-16 22:09:02 +0800

    Corrected audio self.fcycles calculation

--- emulator/src/audio.rs
+++ emulator/src/audio.rs
@@ -360,7 +360,7 @@ impl Tick for Audio {
         */
 
         if self.fcycles >= (self.fcycles_per_sample) {
-            self.fcycles -= self.fcycles_per_sample;
+            self.fcycles -= self.fcycles_per_sample.floor();
 
             /*
             if self.filter_enabled {
