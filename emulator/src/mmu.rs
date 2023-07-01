1b0e1c1d0f0049f6d03be11753db438303f4a2ad
univta0001 <univta0001@gmail.com>
2023-07-01 15:05:41 +0800

    Fix clippy issues in mmu banking code

--- emulator/src/mmu.rs
+++ emulator/src/mmu.rs
@@ -332,10 +332,8 @@ impl Mmu {
         let bank1_mode = (io_addr & 0x08) > 0;
 
         if write_mode {
-            if !write_flag {
-                if self.prewrite {
-                    self.writebsr = true;
-                } 
+            if !write_flag && self.prewrite { 
+                self.writebsr = true;
             } 
             self.prewrite = !write_flag;
             self.readbsr = off_mode;
