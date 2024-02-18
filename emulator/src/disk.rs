f7d80be682a499d9aa0470676ff78aaf1171edf4
univta0001 <univta0001@gmail.com>
2024-02-18 09:14:58 +0800

    Added nuance when reading $c08d,x will reset sequencer and clear contents of data latch based on woz spec

--- emulator/src/disk.rs
+++ emulator/src/disk.rs
@@ -2259,6 +2259,11 @@ impl DiskDrive {
         }
     }
 
+    fn reset_lss(&mut self) {
+        self.lss_state = 0;
+        self.latch &= 0x7f;
+    }
+
     fn step_lss(&mut self) {
         let idx = self.lss_state
             | (self.q7 as u8) << 3
@@ -2492,6 +2497,9 @@ impl Card for DiskDrive {
                 self.q6 = false;
             }
             LOC_DRIVEWRITE => {
+                if !write_mode {
+                    self.reset_lss();
+                }
                 self.q6 = true;
             }
 
