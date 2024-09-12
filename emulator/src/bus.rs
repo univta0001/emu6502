75ac9212844282122e84c1aff3013aba7fc6bb27
univta0001 <univta0001@gmail.com>
2024-09-12 22:37:35 +0800

    Set the reset paddle to 0x80

--- emulator/src/bus.rs
+++ emulator/src/bus.rs
@@ -613,9 +613,9 @@ impl Bus {
     pub fn reset_paddle_latch(&mut self, paddle: usize) {
         if self.joystick_flag {
             if paddle % 2 == 0 {
-                self.paddle_latch[paddle] = (0x7f_i16 + self.paddle_x_trim as i16) as u16;
+                self.paddle_latch[paddle] = (0x80_i16 + self.paddle_x_trim as i16) as u16;
             } else {
-                self.paddle_latch[paddle] = (0x7f_i16 + self.paddle_y_trim as i16) as u16;
+                self.paddle_latch[paddle] = (0x80_i16 + self.paddle_y_trim as i16) as u16;
             }
         } else {
             self.paddle_latch[paddle] = 0xff;
