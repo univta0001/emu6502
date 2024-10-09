2ca2f9ed95c01d1b913cfcfa561e39f40f0a657f
univta0001 <univta0001@gmail.com>
2024-10-09 22:06:01 +0800

    Update dependencies

---
+++ emulator/src/vidhd.rs
@@ -0,0 +1,45 @@
+use crate::bus::Card;
+use crate::mmu::Mmu;
+use crate::video::Video;
+
+#[cfg(feature = "serde_support")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Eq, PartialEq)]
+#[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
+pub struct VidHD;
+
+impl Default for VidHD {
+    fn default() -> Self {
+        VidHD
+    }
+}
+
+impl Card for VidHD {
+    fn rom_access(
+        &mut self,
+        _mem: &mut Mmu,
+        _video: &mut Video,
+        addr: u16,
+        value: u8,
+        _write_flag: bool,
+    ) -> u8 {
+        match addr & 0xff {
+            0 => 0x24,
+            1 => 0xea,
+            2 => 0x4c,
+            _ => value,
+        }
+    }
+
+    fn io_access(
+        &mut self,
+        _mem: &mut Mmu,
+        _video: &mut Video,
+        _addr: u16,
+        _value: u8,
+        _write_flag: bool,
+    ) -> u8 {
+        _value
+    }
+}
