2aab53cd4cb314ec450efcefc5b9e0a38d87ed7a
univta0001 <univta0001@gmail.com>
2025-05-03 17:14:29 +0800

    Fix get track info

--- emulator/src/disk.rs
+++ emulator/src/disk.rs
@@ -1429,7 +1429,7 @@ impl DiskDrive {
         };
         let disk_pos = disk.head * 8 + disk.head_bit;
         let sector = disk_pos / sector_bits;
-        ((disk.last_track / 4) as usize, sector)
+        ((disk.track / 4) as usize, sector)
     }
 
     pub fn get_disk_head_info(&self) -> (usize, usize, usize) {
