cb10b34b7df50fbbef0deab2191907afdc15cf34
univta0001 <univta0001@gmail.com>
2025-11-06 21:46:59 +0800

    update xce implementation

--- emulator/src/cpu65816.rs
+++ emulator/src/cpu65816.rs
@@ -2780,7 +2780,6 @@ impl CPU {
         self.status.p.set(CpuFlags::CARRY, old_e);
 
         if self.e {
-            self.register_a &= 0xff;
             self.register_x &= 0xff;
             self.register_y &= 0xff;
             self.stack_pointer = (0x01 << 8) | (self.stack_pointer & 0xff);
