d3d8682dba643fc2fc431a2ada140bf724108299
univta0001 <univta0001@gmail.com>
2026-05-17 19:26:13 +0800

    Fix potential out of bound in disksound

--- emulator/src/disksound.rs
+++ emulator/src/disksound.rs
@@ -234,8 +234,10 @@ impl DiskSound {
             // Update the stepper sound state
             let sample_bytes = self.sample(&self.step_sample);
             let sample_length = sample_bytes.len();
-            let sample = [sample_bytes[self.step_pos], sample_bytes[self.step_pos + 1]];
-            self.sample_value = self.sample_value.wrapping_add(i16::from_le_bytes(sample));
+            if self.step_pos + 1 < sample_length {
+                let sample = [sample_bytes[self.step_pos], sample_bytes[self.step_pos + 1]];
+                self.sample_value = self.sample_value.wrapping_add(i16::from_le_bytes(sample));
+            }
             self.step_pos += 2;
             if self.step_pos + 1 >= sample_length {
                 self.step_sample = SoundType::Quiet;
